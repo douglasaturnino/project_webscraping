8ef8c1e401aa4d1f662061d03969d94bb456fd5b
Douglas Saturnino <douglasaturnino@gmail.com>
2024-11-15 16:50:01 -0300

    feat(bs4): adiciona o metodo parse_page

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def featch_page():
-    url = "https://lista.mercadolivre.com.br/celulares-telefones/celulares-smartphones/iphone/linha-iphone-16/iphone-16-pro/iphone-16_NoIndex_True#applied_filter_id%3DMODEL%26applied_filter_name%3DModelo%26applied_filter_order%3D3%26applied_value_id%3D41937253%26applied_value_name%3DiPhone+16+Pro%26applied_value_order%3D3%26applied_value_results%3D23%26is_custom%3Dfalse"
+    url = "https://www.mercadolivre.com.br/apple-iphone-16-pro-1-tb-titnio-preto-distribuidor-autorizado/p/MLB1040287851#polycard_client=search-nordic&wid=MLB5054621110&sid=search&searchVariation=MLB1040287851&position=3&search_layout=stack&type=product&tracking_id=98d5335b-6853-403d-9dd6-98374bef3f1b"
     response = requests.get(url)
     return response.text
 
 
+def parse_page(html):
+    soup = BeautifulSoup(html, "html.parser")
+    product_name = soup.find("h1", class_="ui-pdp-title").get_text(strip=True)
+    prices = soup.find_all("span", class_="andes-money-amount__fraction")
+    old_price = int(prices[0].get_text(strip=True).replace(".", ""))
+    new_price = int(prices[1].get_text(strip=True).replace(".", ""))
+    installment_price = int(prices[2].get_text(strip=True).replace(".", ""))
+
+    return {
+        "product_name": product_name,
+        "old_price": old_price,
+        "new_price": new_price,
+        "installment_price": installment_price,
+    }
+
+
 if __name__ == "__main__":
     page_content = featch_page()
+    product_info = parse_page(page_content)
+    print(product_info)
